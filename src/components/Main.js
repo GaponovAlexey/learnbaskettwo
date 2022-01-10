fb7e31a4e55668d91b3a1316c7442daa433d1e0a
Kekelberi <alexei.gaponov@gmail.com>
2022-01-10 18:00:48 +0300

    save

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import logo from './img/1.jpg'
-import logo2 from './img/2.jpg'
 import logo3 from './img/3.jpg'
 
 export const Main = () => {
@@ -21,19 +19,131 @@ export const Main = () => {
                   GPU, 16-core
                 </p>
               </div>
-              <div className="main__price">price</div>
+              <div className="main__price"><del>4000$</del> - 3400$</div>
               <button className='main__button'>Buy</button>
             </div>
             <div className='main__content'>
               <div className='content-imgs'>
-                <img className='content-img' src={logo3} alt='logo2' />
+                <img className='content-img' src={logo3} alt='logo' />
               </div>
+              <p className='main__title'>Macbook 13</p>
+              <div className='main__desc'>
+                <p className='main__desc-full'>
+                  System on Chip (SoC), Apple M1 Pro chip, Up to 10-core CPU
+                  with 8 performance cores and 2 efficiency cores, Upto 16-core
+                  GPU, 16-core
+                </p>
+              </div>
+              <div className="main__price"><del>4000$</del> - 3400$</div>
+              <button className='main__button'>Buy</button>
+            </div>
+
+            <div className='main__content'>
+              <div className='content-imgs'>
+                <img className='content-img' src={logo3} alt='logo' />
+              </div>
+              <p className='main__title'>Macbook 16</p>
+              <div className='main__desc'>
+                <p className='main__desc-full'>
+                  System on Chip (SoC), Apple M1 Pro chip, Up to 10-core CPU
+                  with 8 performance cores and 2 efficiency cores, Upto 16-core
+                  GPU, 16-core
+                </p>
+              </div>
+              <div className="main__price"><del>4000$</del> - 3400$</div>
+              <button className='main__button'>Buy</button>
             </div>
             <div className='main__content'>
               <div className='content-imgs'>
-                <img className='content-img' src={logo3} alt='logo3' />
+                <img className='content-img' src={logo3} alt='logo' />
               </div>
+              <p className='main__title'>Macbook 16</p>
+              <div className='main__desc'>
+                <p className='main__desc-full'>
+                  System on Chip (SoC), Apple M1 Pro chip, Up to 10-core CPU
+                  with 8 performance cores and 2 efficiency cores, Upto 16-core
+                  GPU, 16-core
+                </p>
+              </div>
+              <div className="main__price"><del>4000$</del> - 3400$</div>
+              <button className='main__button'>Buy</button>
             </div>
+            <div className='main__content'>
+              <div className='content-imgs'>
+                <img className='content-img' src={logo3} alt='logo' />
+              </div>
+              <p className='main__title'>Macbook 16</p>
+              <div className='main__desc'>
+                <p className='main__desc-full'>
+                  System on Chip (SoC), Apple M1 Pro chip, Up to 10-core CPU
+                  with 8 performance cores and 2 efficiency cores, Upto 16-core
+                  GPU, 16-core
+                </p>
+              </div>
+              <div className="main__price"><del>4000$</del> - 3400$</div>
+              <button className='main__button'>Buy</button>
+            </div>
+            <div className='main__content'>
+              <div className='content-imgs'>
+                <img className='content-img' src={logo3} alt='logo' />
+              </div>
+              <p className='main__title'>Macbook 16</p>
+              <div className='main__desc'>
+                <p className='main__desc-full'>
+                  System on Chip (SoC), Apple M1 Pro chip, Up to 10-core CPU
+                  with 8 performance cores and 2 efficiency cores, Upto 16-core
+                  GPU, 16-core
+                </p>
+              </div>
+              <div className="main__price"><del>4000$</del> - 3400$</div>
+              <button className='main__button'>Buy</button>
+            </div>
+            <div className='main__content'>
+              <div className='content-imgs'>
+                <img className='content-img' src={logo3} alt='logo' />
+              </div>
+              <p className='main__title'>Macbook 16</p>
+              <div className='main__desc'>
+                <p className='main__desc-full'>
+                  System on Chip (SoC), Apple M1 Pro chip, Up to 10-core CPU
+                  with 8 performance cores and 2 efficiency cores, Upto 16-core
+                  GPU, 16-core
+                </p>
+              </div>
+              <div className="main__price"><del>4000$</del> - 3400$</div>
+              <button className='main__button'>Buy</button>
+            </div>
+            <div className='main__content'>
+              <div className='content-imgs'>
+                <img className='content-img' src={logo3} alt='logo' />
+              </div>
+              <p className='main__title'>Macbook 16</p>
+              <div className='main__desc'>
+                <p className='main__desc-full'>
+                  System on Chip (SoC), Apple M1 Pro chip, Up to 10-core CPU
+                  with 8 performance cores and 2 efficiency cores, Upto 16-core
+                  GPU, 16-core
+                </p>
+              </div>
+              <div className="main__price"><del>4000$</del> - 3400$</div>
+              <button className='main__button'>Buy</button>
+            </div>
+            <div className='main__content'>
+              <div className='content-imgs'>
+                <img className='content-img' src={logo3} alt='logo' />
+              </div>
+              <p className='main__title'>Macbook 16</p>
+              <div className='main__desc'>
+                <p className='main__desc-full'>
+                  System on Chip (SoC), Apple M1 Pro chip, Up to 10-core CPU
+                  with 8 performance cores and 2 efficiency cores, Upto 16-core
+                  GPU, 16-core
+                </p>
+              </div>
+              <div className="main__price"><del>4000$</del> - 3400$</div>
+              <button className='main__button'>Buy</button>
+            </div>
+          
           </div>
         </div>
       </div>
